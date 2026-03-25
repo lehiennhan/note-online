0976be0f49bf2ec6e9bf7b27f8f8ebecc1afb8a9
nhaan lee <hiennhan@vn-cubesystem.com>
2026-03-25 19:23:03 +0700

    change my info

--- app/compare-text/page.tsx
+++ app/compare-text/page.tsx
@@ -19,7 +19,7 @@ export default function CompareTextPage() {
 
     const handleScroll = (
         event: React.UIEvent<HTMLTextAreaElement>,
-        lineRef: React.RefObject<HTMLDivElement>
+        lineRef: React.RefObject<HTMLDivElement | null>
     ) => {
         if (lineRef.current && event.currentTarget) {
             lineRef.current.scrollTop = event.currentTarget.scrollTop;
